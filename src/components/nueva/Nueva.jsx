e54a4f6a10b42eccdb63d65fc30c82994773c61d
Kenneth32Mena <kenneth2024@hotmail.com>
2026-08-25 22:56:16 -0600

    HTML para detalles.jsx

--- src/components/nueva/Nueva.jsx
+++ src/components/nueva/Nueva.jsx
@@ -1,9 +1,50 @@
 function Nueva() {
-    return ( 
-        <>
-        
-        </>
-     );
+    const frecuencias = ["día", "semana", "mes", "año"];
+    const iconos = ["💻", "🏃‍♂️", "📚", "✈️", "💵"];
+
+
+    return (
+        <div>
+            <form action="#">
+                <label>
+                    Describe tu meta
+                    <input placeholder="ej. 52 caminatas" />
+                </label>
+                <label>
+                    ¿CON QUE FRECUENCIA DESEAS CUMPLIR TU META?(ej. 1 vez a la semana)
+                    <input type="number" />
+                    <select>
+                        {frecuencias.map(frecuencia=> <option value={frecuencia}>{frecuencia}</option>)}
+                    </select>
+                </label>
+                <label>
+                    ¿CUANTAS VECES DESEAS COMPLETAR ESTA META?
+                    <input type="number" />
+                </label>
+                <label>
+                    ¿TIENE UNA FECHA LIMITE?
+                    <input type="date" />
+                </label>
+                <label>
+                    ¿CUANTAS VECES HAZ COMPLETADO YA ESTA META
+                    <input type="number" />
+
+                </label>
+                <label>
+                    ESCOGE EL ICONO PARA LA META
+                    <select>
+                        {iconos.map(icono => <option value={icono}>{icono}</option>)}
+
+                    </select>
+                </label>
+            </form>
+            <div>
+                <button></button>
+
+                <button></button>
+            </div>
+        </div>
+    );
 }
 
 export default Nueva;
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import Principal  from './components/compartidos/Principal.jsx';
 import Encabezado from './components/compartidos/Encabezado.jsx';
 import Pie from './components/compartidos/Pie.jsx';
 import Lista from './components/lista/Lista.jsx';
+import Nueva from './components/nueva/Nueva.jsx';
 let root =createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <Encabezado/>
     <Principal>
-    <Lista/>
+    <Nueva/>
     </Principal>
     <Pie/>
   </StrictMode>
